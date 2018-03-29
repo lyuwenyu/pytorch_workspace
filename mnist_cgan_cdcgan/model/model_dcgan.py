a5b7287482b0b108ffdc99c89940d28510edcb9d
lyuwenyu <wenyu.lyu@gmail.com>
2018-03-29 14:16:29 +0800

    cdcgan

---
+++ mnist_cgan_cdcgan/model/model_dcgan.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn 
+import torch.nn.functional as F 
+from torch.autograd import Variable
+
+def _weight_init(mm):
+    pass
+
+
+class generator(nn.Module):
+    
+    def __init__(self, z_size=100, y_size=10):
+        super(generator, self).__init__()
+
+        self.deconv1 = nn.ConvTranspose2d(z_size+y_size, 256, kernel_size=5, stride=1)
+        self.bn1 = nn.BatchNorm2d(256)
+
+        self.deconv2 = nn.ConvTranspose2d(256, 128, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(128)
+
+        self.deconv3 = nn.ConvTranspose2d(128, 1, kernel_size=4, stride=2)
+
+
+
+    def forward(self, z, y):
+
+        out = torch.cat([z, y], dim=1)  # [batch, 110, 1, 1]
+
+        out = F.relu( self.deconv1(out) )
+        # out = self.bn1(out)
+
+        out = F.relu( self.deconv2(out) )
+        # out = self.bn2(out)
+
+        out = self.deconv3(out)
+
+        return F.tanh(out)
+
+
+
+class discriminator(nn.Module):
+
+    def __init__(self, x_size=1, y_size=10):
+        super(discriminator, self).__init__()
+
+        self.conv1 = nn.Conv2d(11, 128, kernel_size=5, stride=2)
+        
+        self.conv2 = nn.Conv2d(128, 256, kernel_size=5, stride=2)
+        self.bn = nn.BatchNorm2d(256)
+
+        self.conv3 = nn.Conv2d(256, 1, kernel_size=4, stride=1)
+
+
+    def forward(self, x, y): # n 1 28 28,  n 10 28 28
+
+        out = torch.cat([x, y], dim=1)
+
+        out = F.relu( self.conv1(out) )
+
+        out = F.relu( self.conv2(out) )
+        # out = self.bn(out)
+        
+        out = self.conv3(out)
+
+        return F.sigmoid(out)
+
+
+
+if __name__ == '__main__':
+
+
+    gg = generator()
+    dd = discriminator()
+
+    x = Variable( torch.randn(10, 1, 28, 28))
+    z = Variable( torch.randn(10, 100, 1, 1) )
+    y = Variable( torch.randn(10, 10, 1, 1) )
+    y_ = y + Variable( torch.zeros(10, 10, 28,28) )
+
+    print(gg(z, y).size())
+    print(dd(x, y_).size())
---
+++ mnist_cgan_cdcgan/train_dcgan.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from model import model_dcgan 
+import torch.utils.data as data 
+import numpy as np
+from data.dataset import MNIST
+import torchvision.utils as vutils
+
+batch_size = 60
+
+mnist = MNIST()
+dataloader = data.DataLoader(mnist, batch_size=batch_size, shuffle=True)
+
+gnet = model_dcgan.generator()
+dnet = model_dcgan.discriminator()
+
+g_optim = optim.Adam(gnet.parameters(), lr=0.002)
+d_optim = optim.Adam(dnet.parameters(), lr=0.002)
+
+crit = nn.BCELoss()
+
+
+fixed_z = Variable( torch.randn(100, 100, 1, 1))
+fixed_y = Variable( torch.cat([torch.eye(10,10)]*10, dim=0).view(100, 10, 1, 1))
+# fixed_y_exp = Variable( fixed_y + Variable(torch.zeros(100, 10, 28, 28)))
+
+for e in range(100):
+
+    # dnet.train()
+    gnet.train()
+
+    for i, (imgs, labs) in enumerate(dataloader):
+
+        # train d
+        for _ in range(2):
+            
+            x = Variable(imgs.float().view(batch_size, 1, 28,28))
+            y = Variable(labs.float().view(batch_size, -1, 1, 1))
+
+        
+            y_exp = Variable( torch.zeros(batch_size, 10, 28,28) ) + y
+            z = Variable( torch.randn(batch_size, 100, 1, 1))
+
+            d_real = dnet(x, y_exp)
+            
+            fake = gnet(z, y)
+            fake.detach()
+            d_fake = dnet(fake, y_exp)
+
+            d_loss = crit(d_real, Variable( torch.ones(batch_size,1,1,1).float() ) )  + \
+                        crit(d_fake, Variable( torch.zeros(batch_size,1,1,1).float() ))
+
+            d_optim.zero_grad()
+            d_loss.backward()
+            d_optim.step()
+
+
+        # train g
+
+        z = Variable( torch.randn(batch_size, 100, 1, 1))
+
+        y = torch.eye(10)
+        index = np.random.randint(0,10, (batch_size))
+        y = y[ torch.from_numpy(index) ]
+        y = Variable( y.view(batch_size, -1, 1, 1) )
+
+        y_exp = Variable( torch.zeros(batch_size, 10, 28,28) ) + y
+
+
+        fake = gnet(z, y)
+        d_fake = dnet(fake, y_exp)
+
+        g_loss = crit(d_fake, Variable( torch.ones(batch_size,1,1,1).float() ))
+
+
+        g_optim.zero_grad()
+        g_loss.backward()
+        g_optim.step()
+
+
+        if i%50 == 0:
+            print(d_loss.data.numpy(), g_loss.data.numpy())
+
+    gnet.eval()
+    vutils.save_image( gnet(fixed_z, fixed_y) , './output/dcgan_{:0>3}.jpg'.format(e), nrow=10)
