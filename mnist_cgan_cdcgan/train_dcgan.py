b83667fd20f1410a01e54d3c057d2a75f42f2921
lyuwenyu <wenyu.lyu@gmail.com>
2018-03-30 12:41:38 +0800

    train dcgan

--- mnist_cgan_cdcgan/train_dcgan.py
+++ mnist_cgan_cdcgan/train_dcgan.py
@@ -16,15 +16,24 @@ dataloader = data.DataLoader(mnist, batch_size=batch_size, shuffle=True)
 gnet = model_dcgan.generator()
 dnet = model_dcgan.discriminator()
 
+if torch.cuda.is_available():
+    gnet = gnet.cuda()
+    dnet = dnet.cuda()
+
 g_optim = optim.Adam(gnet.parameters(), lr=0.0002, betas=(0.5, 0.99))
 d_optim = optim.Adam(dnet.parameters(), lr=0.0002, betas=(0.5, 0.99))
 
 crit = nn.BCELoss()
-
+if torch.cuda.is_available():
+    crit = crit.cuda()
 
 fixed_z = Variable( torch.randn(100, 100, 1, 1))
 fixed_y = Variable( torch.cat([torch.eye(10,10)]*10, dim=0).view(100, 10, 1, 1))
 # fixed_y_exp = Variable( fixed_y + Variable(torch.zeros(100, 10, 28, 28)))
+if torch.cuda.is_available():
+    fixed_y = fixed_y.cuda()
+    fixed_z = fixed_z.cuda()
+
 
 for e in range(100):
 
@@ -45,14 +54,25 @@ for e in range(100):
             # z = Variable( torch.randn(batch_size, 100, 1, 1))
             z = Variable( (torch.rand(batch_size, 100, 1, 1)-0.5)/0.5 )
 
+            if torch.cuda.is_available():
+                x = x.cuda()
+                y = y.cuda()
+                y_exp = y_exp.cuda()
+                z = z.cuda()
+
+
             d_real = dnet(x, y_exp)
             
             fake = gnet(z, y)
             fake.detach()
             d_fake = dnet(fake, y_exp)
 
-            d_loss = crit(d_real, Variable( torch.ones(batch_size,1).float() ) )  + \
-                        crit(d_fake, Variable( torch.zeros(batch_size,1).float() ))
+            if torch.cuda.is_available()
+                d_loss = crit(d_real, Variable( torch.ones(batch_size,1).float().cuda() ) )  + \
+                            crit(d_fake, Variable( torch.zeros(batch_size,1).float().cuda() ))
+            else:
+                d_loss = crit(d_real, Variable( torch.ones(batch_size,1).float() ) )  + \
+                            crit(d_fake, Variable( torch.zeros(batch_size,1).float() ))
 
             d_optim.zero_grad()
             d_loss.backward()
@@ -72,11 +92,21 @@ for e in range(100):
         y_exp = Variable( torch.zeros(batch_size, 10, 28,28) ) + y
 
 
+        if torch.cuda.is_available():
+            z = z.cuda()
+            y = y.cuda()
+            y_exp = y_exp.cuda()
+
         fake = gnet(z, y)
         d_fake = dnet(fake, y_exp)
 
-        g_loss = crit(d_fake, Variable( torch.ones(batch_size,1).float() ))
+        if torch.cuda.is_available():
 
+            g_loss = crit(d_fake, Variable( torch.ones(batch_size,1).float().cuda() ))
+
+        else:
+
+            g_loss = crit(d_fake, Variable( torch.ones(batch_size,1).float() ))
 
         g_optim.zero_grad()
         g_loss.backward()
@@ -84,8 +114,8 @@ for e in range(100):
 
 
         if i%20 == 0:
-            print(e, i, d_loss.data.numpy(), g_loss.data.numpy())
+            print(e, i, d_loss.cpu().data.numpy(), g_loss.cpu().data.numpy())
 
             gnet.eval()
             fake = gnet(fixed_z, fixed_y)
-            vutils.save_image( fake.data , './output/gn_dcgan_{:0>3}.jpg'.format(e), nrow=10)
+            vutils.save_image( fake.cpu().data , './output/gn_dcgan_{:0>3}.jpg'.format(e), nrow=10)
