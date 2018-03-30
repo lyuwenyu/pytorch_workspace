dae706a3a8c41a708f5de5abdc558e4a6fca5a09
lyuwenyu <wenyu.lyu@gmail.com>
2018-03-30 11:24:28 +0800

    train dcgan

--- mnist_cgan_cdcgan/train_dcgan.py
+++ mnist_cgan_cdcgan/train_dcgan.py
@@ -16,8 +16,8 @@ dataloader = data.DataLoader(mnist, batch_size=batch_size, shuffle=True)
 gnet = model_dcgan.generator()
 dnet = model_dcgan.discriminator()
 
-g_optim = optim.Adam(gnet.parameters(), lr=0.002)
-d_optim = optim.Adam(dnet.parameters(), lr=0.002)
+g_optim = optim.Adam(gnet.parameters(), lr=0.0002, betas=(0.5, 0.99))
+d_optim = optim.Adam(dnet.parameters(), lr=0.0002, betas=(0.5, 0.99))
 
 crit = nn.BCELoss()
 
@@ -42,7 +42,8 @@ for e in range(100):
             y = Variable(labs.float().view(batch_size, 10, 1, 1))
         
             y_exp = Variable( torch.zeros(batch_size, 10, 28,28) ) + y
-            z = Variable( torch.randn(batch_size, 100, 1, 1))
+            # z = Variable( torch.randn(batch_size, 100, 1, 1))
+            z = Variable( (torch.rand(batch_size, 100, 1, 1)-0.5)/0.5 )
 
             d_real = dnet(x, y_exp)
             
@@ -60,7 +61,8 @@ for e in range(100):
 
         # train g
 
-        z = Variable( torch.randn(batch_size, 100, 1, 1))
+        # z = Variable( torch.randn(batch_size, 100, 1, 1))
+        z = Variable( (torch.rand(batch_size, 100, 1, 1)-0.5)/0.5 )
 
         y = torch.eye(10)
         index = np.random.randint(0,10, (batch_size))
@@ -86,4 +88,4 @@ for e in range(100):
 
             gnet.eval()
             fake = gnet(fixed_z, fixed_y)
-            vutils.save_image( fake.data , './output/dcgan_{:0>3}.jpg'.format(e), nrow=10)
+            vutils.save_image( fake.data , './output/gn_dcgan_{:0>3}.jpg'.format(e), nrow=10)
