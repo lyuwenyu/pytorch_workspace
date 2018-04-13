1fb01ec4c227131839533ff2d0ba86ee8307fb45
lyuwenyu <wenyu.lyu@gmail.com>
2018-04-13 15:19:20 +0800

    data augmentation

--- augmentor/pytorch_transforms.py
+++ augmentor/pytorch_transforms.py
@@ -17,13 +17,13 @@ class DatasetX(data.Dataset):
 
     def __init__(self, path='/home/wenyu/Desktop/test', use_augmentor=True, is_training=True):
 
-
+        ## params
         self.use_augmentor = use_augmentor
         self.is_training = is_training
 
+        ## paths
         self.imgs = glob.glob(path + '/**/**/*.png')
 
-        # self.p = Augmentor.Pipeline()
 
         if use_augmentor:
 
@@ -31,7 +31,8 @@ class DatasetX(data.Dataset):
             # self.transforms = transforms.Compose([ self.p.torch_transform(), transforms.ToTensor() ])
 
 
-        self.pre_process = transforms.Compose([ transforms.ToTensor(), 
+        self.preprocess = transforms.Compose([ transforms.Resize(size=[224,224]),
+                                                transforms.ToTensor(), 
                                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
 
 
@@ -46,18 +47,23 @@ class DatasetX(data.Dataset):
         
         img = Image.open(self.imgs[i]).convert('RGB')
 
+        imgs = [ img ]
+
         if self.is_training:
             
-            # im = self.transforms(img)
-            
-            # im = self.p._execute_with_array( np.array(img) )
+            if self.use_augmentor:
+                
+                imgs = [ Image.fromarray(self.p._execute_with_array(np.array(x))) for x in imgs ]
 
-            img = self._transforms([img])
-            
-            
-        ims = [self.pre_process(x) for x in img]
+            else:
 
-        return ims
+                imgs = self._transforms(imgs)
+
+
+        imgs = [ self.preprocess(x) for x in imgs ]
+
+
+        return imgs
 
 
 
@@ -66,6 +72,9 @@ class DatasetX(data.Dataset):
         p = Augmentor.Pipeline()
 
         p.rotate(probability=0.5, max_left_rotation=10, max_right_rotation=10)
+        p.resize(probability=1.0, width=224, height=224)
+        p.flip_left_right(probability=0.5)
+
 
         return p
 
@@ -73,7 +82,8 @@ class DatasetX(data.Dataset):
     def _transforms(self, images=[]):
         
         if not isinstance(images, list):
-            raise RuntimeError
+            
+            images = [images]
 
         params = transforms.RandomCrop.get_params(images[0], output_size=[224, 224]) ## get parameters
         images = [ transformsF.crop(im, *params) for im in images ]
@@ -91,7 +101,7 @@ if __name__ == '__main__':
     dataset = DatasetX()
 
     
-    dataset_loader = data.DataLoader(dataset, batch_size=3, num_workers=1) 
+    dataset_loader = data.DataLoader(dataset, batch_size=10, num_workers=5) 
 
     for d in dataset_loader:
 
