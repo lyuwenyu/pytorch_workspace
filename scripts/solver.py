47c7c2a32c756b28d408bdef0f30883d0713294c
lyuwenyu <wenyu.lyu@gmail.com>
2018-04-24 11:53:33 +0800

    solver

--- scripts/solver.py
+++ scripts/solver.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
 
 from collections import OrderedDict
 import logging
@@ -15,9 +17,12 @@ class Solver(object):
 
     def __init__(self, model=None, criteria=None, epoches=10):
 
-        self.model = model
+        self.model = nn.Conv2d(10, 20, 3)
         self.criteria = criteria
-        self.scheduler = ''
+
+        self.optimizer = optim.SGD(self.model.parameters(), lr=0.1, momentum=0.9)
+        self.scheduler = lr_scheduler.StepLR(self.optimizer, step_size=2, gamma=0.1)
+        
 
         self.epoches = epoches
         self.current_epoch = 0
@@ -40,11 +45,15 @@ class Solver(object):
 
     def train(self, data_loader):
         # print(self.current_epoch)
+        self.model.train()
+
         self.current_epoch += 1
         self.logger.info(233)
 
 
     def test(self, data_loader):
+        self.model.eval()
+
         self.logger.info('----')
         self.logger.info('test')
         self.logger.info('----')
@@ -52,54 +61,41 @@ class Solver(object):
 
     def run(self, train_data_loader=None, test_data_loader=None):
 
-        for i in range(self.epoches):
-
+        for i in range(0, self.epoches):
             self.train(train_data_loader)
             self.test(test_data_loader)
 
-            if i % 5:
+            if i % 3 == 0:
                 self.save(prefix='{:0>3}'.format(i))
         
+            self.scheduler.step()
+
+            print(i)
+
         self.save(prefix='final')
 
 
     def save(self, prefix=''):
 
-        solver_path = '{}_solver.pt'.format(prefix)
-        # model_path = '{}_model.pt'.format(prefix)
-        # scheduler_path = '{}_scheduelr.pt'.format(prefix)
-
-        # torch.save(self.model.state_dict(), model_path)
-        
-        with open(solver_path, 'wb') as f:
-            pickle.dump(self.state_dict(), f)
-        # torch.save(self.state_dict(), solver_path)
-
-        # scheduler
-        # torch.save(self.scheduler.state_dict(), scheduler_path)
+        state = {
+            'params': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'solver': self.state_dict()
+        }
+        torch.save(state, prefix+'.pt')
 
 
-    def restore(self, solver_path=None, model_path=None, scheduler_path=None):
+    def restore(self, path):
 
-        # solver_path = '{}_solver'.format(prefix)
-        # model_path = '{}_model'.format(prefix)
-        # scheduler_path = '{}_scheduelr'.format(prefix)
-
-        if os.path.exists(solver_path):
-            with open(solver_path, 'rb') as f:
-                self.load_state_dict(pickle.load(f))
-            # self.load_state_dict(torch.load(solver_path))
-
-        # if os.path.exists(model_path):
-        #     self.model.load_state_dict(torch.load(model_path))
-                
-        # if os.path.exists(scheduler_path):
-        #     self.scheduler.load_state_dict(torch.load(scheduler_path))
+        state = torch.load(path)
+        self.optimizer.load_state_dict(state['optimizer'])
+        self.model.load_state_dict(state['params'])
+        self.load_state_dict(state['solver'])
 
 
     def state_dict(self, ):
 
-        # print(self.__dict__.keys())
+        # print(self.__dict__.keys())epoches
 
         des = OrderedDict()
 
@@ -109,6 +105,10 @@ class Solver(object):
             if 'logger' == k:
                 continue
             if 'criteria' == k:
+                continue
+            if 'scheduler' == k:
+                continue
+            if 'optimizer' == k:
                 continue
             des[k] = self.__dict__[k]    
             
@@ -129,9 +129,13 @@ if __name__ == '__main__':
 
     solver = Solver()
 
-    solver.run()
+    print( solver.optimizer.state_dict() )
+    solver.restore(path='005.pt')
+    print( solver.optimizer.state_dict() )
 
-    # solver.restore('final')
+    solver.run()
+    print( solver.optimizer.state_dict() )
+
     # print(solver.current_epoch)
     # print(solver.__dict__)
 
@@ -143,10 +147,27 @@ if __name__ == '__main__':
 
     # print(solver.current_epoch)
 
-    torch.save( solver.state_dict(), 'test' )
-    solver.load_state_dict( torch.load('test') )
-    print(solver.current_epoch)
+    # torch.save( solver.state_dict(), 'test' )
+    # solver.load_state_dict( torch.load('test') )
+    
+    #print(solver.current_epoch)
 
     # print( pickle.dumps(solver) )
     # solver1 = pickle.loads( pickle.dumps(solver) )
     # print(solver.current_epoch)
+
+    ###
+
+    # m = nn.Conv2d(10, 20, 3)
+    # optimizer = optim.SGD(m.parameters(), lr=0.1, momentum=0.9)
+
+    # print( optimizer.state_dict() )
+    
+    # optimizer.load_state_dict( optimizer.state_dict() )
+
+    # scheduler = lr_scheduler.StepLR(optimizer, step_size=5)
+    
+    # for i in range(20):
+    #     scheduler.step()
+
+    # print( optimizer.state_dict() )
