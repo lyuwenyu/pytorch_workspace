46e1c6ae544e363184754eaca1c8bc6895b50d16
lyuwenyu <wenyu.lyu@gmail.com>
2018-08-28 18:54:30 +0800

    argparse test 2

--- python/argparse_test.py
+++ python/argparse_test.py
@@ -1,9 +1,10 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='test')
-parser.add_argument('--n', type=int, nargs='+', help='ss')
+parser.add_argument('--n', type=int, nargs='+', help='ss') # nargs='?'
 parser.add_argument('--s', type=str, default='ss')
 args = parser.parse_args()
+  
 
 
 if __name__ == '__main__':
