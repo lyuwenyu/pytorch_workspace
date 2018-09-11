53f5460b4e3439cff1e2e962b95cb7f06acc20a1
lyuwenyu <wenyu.lyu@gmail.com>
2018-09-11 11:31:18 +0800

    add xyxy xywh normalize options.

--- yolov3/utils/ops_show_bbox.py
+++ yolov3/utils/ops_show_bbox.py
@@ -1,11 +1,35 @@
+import numpy as np
 from PIL import Image
 from PIL import ImageDraw
 
 
-def show_bbox(img, bbox=None, names=None):
+def show_bbox(img, bbox=None, names=None, xyxy=True, normalize=True):
 
     draw = ImageDraw.Draw(img)
-    print(bbox)
-    for bb in bbox:
+    bbox = np.array(bbox)
+    if xyxy:
+        
+        if normalize:
+            bbox[:, [0, 2]] *= img.size[0]
+            bbox[:, [1, 3]] *= img.size[1]
+    
+        new_bbox = bbox
+
+    else:
+
+        new_bbox = np.zeros_like(bbox)
+        new_bbox[:, 0] = bbox[:, 0] - bbox[:, 2] / 2
+        new_bbox[:, 1] = bbox[:, 1] - bbox[:, 3] / 2
+        new_bbox[:, 2] = bbox[:, 0] + bbox[:, 2] / 2
+        new_bbox[:, 3] = bbox[:, 1] + bbox[:, 3] / 2
+
+        if normalize:
+            new_bbox[:, [0, 2]] *= img.size[0]
+            new_bbox[:, [1, 3]] *= img.size[1]
+            # new_bbox[:, 2] *= img.size[0]
+            # new_bbox[:, 3] *= img.size[1]
+    
+    for bb in new_bbox:
         draw.rectangle(tuple(bb), outline='red')
+
     img.show()
