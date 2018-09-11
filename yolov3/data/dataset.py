4b91a6502bb074f190e69e77bb9b3947b7104e4a
lyuwenyu <wenyu.lyu@gmail.com>
2018-09-11 11:02:35 +0800

    tensor do not declare in __init__

--- yolov3/data/dataset.py
+++ yolov3/data/dataset.py
@@ -12,11 +12,12 @@ import torchvision.transforms as transforms
 
 from utils.ops_parse_xml import parse_xml
 from utils.ops_pad_resize import pad_resize
+from utils.ops_show_bbox import show_bbox
 
 from utils import ops_transform
 
 class Dataset(data.Dataset):
-    def __init__(self, path, size=512, yolo=True):
+    def __init__(self, path, size=512, num_classes=20):
         
         root = '/home/wenyu/workspace/dataset/voc/VOCdevkit/VOC2007'
         self.img_root = os.path.join(root, 'JPEGImages')
@@ -24,46 +25,56 @@ class Dataset(data.Dataset):
 
         self.totensor = transforms.ToTensor()
 
-        self.bboxes_tensor = torch.ones(50, 5)
-
     def __len__(self):
         return len(self.anns)
     
     def __getitem__(self, i):
         
         blob = parse_xml(self.anns[i])
-
         filename = os.path.join(self.img_root, blob['filename'])
+        bboxes = blob['bboxes']
+        ngt = len(bboxes)
+        classes = blob['names']
+        assert len(classes) == ngt, ''
 
         img = Image.open(filename)
-        bboxes = blob['bboxes']
 
-        img, bboxes = pad_resize(img, bboxes)
-        
         if random.random() < 0.5:
             img, bboxes = ops_transform.flip_lr(img, bboxes)
 
         if random.random() < 0.5:
             img, bboxes = ops_transform.flip_tb(img, bboxes)
 
-        img, bboxes = ops_transform.pad_resize(img, bboxes, size=(600, 600))
+        img, bboxes = ops_transform.pad_resize(img, bboxes, size=(512, 512))
         bboxes = ops_transform.xyxy2xywh(bboxes, img.size)
 
         img = self.totensor(img)
-        self.bboxes_tensor.fill_(1)
-        self.bboxes_tensor[:len(bboxes), 1:] = torch.from_numpy(bboxes)
-        self.bboxes_tensor[len(bboxes): , 0] = 0
 
-        return img, self.bboxes_tensor
+        bboxes_tensor = torch.zeros(50, 5)
+        bboxes_tensor[:ngt, 1: ] = torch.from_numpy(bboxes)
+        bboxes_tensor[:ngt, 0] = 1
+
+        
+
+        return img, bboxes_tensor
 
 
 if __name__ == '__main__':
     
 
     dataset = Dataset('')
-    dataloader = data.DataLoader(dataset, batch_size=10, shuffle=True, num_workers=5) 
+    dataloader = data.DataLoader(dataset, batch_size=3, shuffle=True, num_workers=2) 
     
+    topil = transforms.ToPILImage()
+
     for img, bboxes in dataloader:
 
-        print(img.size(), bboxes.size())
-        print(bboxes)
+        _img = topil(img[2])
+        _bbox = bboxes[2]
+        _bbox = _bbox[_bbox[:, 0] > 0]
+        _bbox = _bbox[:, 1:]
+
+        show_bbox(_img, ops_transform.xywh2xyxy(_bbox, _img.size))
+
+
+
