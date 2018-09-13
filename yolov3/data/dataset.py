237ec6cde85a933b736cc21ada1bb6a1cff3dbf5
lyuwenyu <wenyu.lyu@gmail.com>
2018-09-13 22:38:33 +0800

    update dataset test code

--- yolov3/data/dataset.py
+++ yolov3/data/dataset.py
@@ -13,19 +13,26 @@ import torchvision.transforms as transforms
 from utils.ops_parse_xml import parse_xml
 from utils.ops_pad_resize import pad_resize
 from utils.ops_show_bbox import show_bbox
+from utils.ops_show_bbox import show_tensor_bbox
 
 from utils import ops_transform
 
 class Dataset(data.Dataset):
     def __init__(self, annos_dir='', image_dir='', classes_path='', size=512, num_classes=20):
         
-        # root = '/home/wenyu/workspace/dataset/voc/VOCdevkit/VOC2007'
-        # self.img_root = os.path.join(root, 'JPEGImages')
-        # self.anns = glob.glob(os.path.join(root, 'Annotations', '*.xml'))
-        
-        # with open('./data/voc.names', 'r') as f:
-        with open('./data/fisheye.names', 'r') as f:
 
+        self._set_costum_dataset()
+
+        self.size = size
+        self.totensor = transforms.ToTensor()
+
+    def _set_costum_dataset(self, ):
+        ''' init
+        self.anns
+        self.image_dir
+        self.label_map
+        '''
+        with open('./data/fisheye.names', 'r') as f:
             lines = f.readlines()
             lines = [ll.strip() for ll in lines]
             self.label_map = dict(zip(lines, range(len(lines))))
@@ -34,37 +41,57 @@ class Dataset(data.Dataset):
         self.image_dir = annos_dir
         self.anns = glob.glob(os.path.join(annos_dir, '*.xml'))
 
-        self.size = size
-        self.totensor = transforms.ToTensor()
+        self.max_n = 50
+
+    def _set_voc_dataset(self, ):
+        ''' init
+        self.anns
+        self.image_dir
+        self.label_map
+        '''
+        # root = '/home/wenyu/workspace/dataset/voc/VOCdevkit/VOC2007'
+        # self.img_root = os.path.join(root, 'JPEGImages')
+        # self.anns = glob.glob(os.path.join(root, 'Annotations', '*.xml'))
+        # with open('./data/voc.names', 'r') as f:
+
 
     def __len__(self):
         return len(self.anns)
     
     def __getitem__(self, i):
-        
+        '''
+        return:
+        image: c, h, w (totensor)
+        bboxes: n 6, [mask-0/1, label-0/cls, bbox-4/x/y/h/w]
+        '''
         blob = parse_xml(self.anns[i])
         path = os.path.join(self.image_dir, blob['filename'])
+
         bboxes = blob['bboxes']
-        ngt = len(bboxes)
         label = [self.label_map[n] for n in blob['names']] 
-        # assert len(label) == ngt, ''
-
         img = Image.open(path)
-
+        # ngt = len(bboxes)
+        
         if random.random() < 0.5:
             img, bboxes = ops_transform.flip_lr(img, bboxes)
 
-        if random.random() < 0.5:
+        if random.random() < 0.2:
             img, bboxes = ops_transform.flip_tb(img, bboxes)
 
         if random.random() < 0.5:
             img, bboxes = ops_transform.pad_resize(img, bboxes, size=(self.size, self.size))
 
+        if random.random() < 0.5:
+            img, bboxes, label = ops_transform.random_perspective(img, bboxes, label)
+
         img, bboxes = ops_transform.resize(img, bboxes, size=(self.size, self.size))
 
-        bboxes = ops_transform.xyxy2xywh(bboxes, img.size)     
+        # here to yolo bbox type
+        bboxes = ops_transform.xyxy2xywh(bboxes, img.size)  
+
         # mask cls bbx
-        target_tensor = torch.zeros(50, 6)
+        ngt = len(bboxes)
+        target_tensor = torch.zeros(self.max_n, 6)
         target_tensor[:ngt, 2: ] = torch.from_numpy(bboxes)
         target_tensor[:ngt, 1] = torch.tensor(label)
         target_tensor[:ngt, 0] = 1
@@ -84,16 +111,6 @@ if __name__ == '__main__':
 
     for img, bboxes in dataloader:
 
-        # print(img.size(), bboxes.size())
+        show_tensor_bbox(img[0], bboxes[0])
 
-        _img = topil(img[0])
-        _bboxes = bboxes[0]
-        _bboxes = _bboxes[_bboxes[:, 0] > 0]
-        _bbox = _bboxes[:, 2:]
-        _classes = _bboxes[:, 1]
-
-        # show_bbox(_img, ops_transform.xywh2xyxy(_bbox, _img.size), normalize=False)
-        # show_bbox(_img, _bbox, xyxy=False, normalize=True)
-        
-        print(_classes)
-        # break
+        break
