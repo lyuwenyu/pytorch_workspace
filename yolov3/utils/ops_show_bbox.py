af46e7641d254e38914b9e2f6270da7924b1bdf7
lyuwenyu <wenyu.lyu@gmail.com>
2018-09-13 22:11:09 +0800

    add show tensor image/bbox func.(verify correctness for data augmentation)

--- yolov3/utils/ops_show_bbox.py
+++ yolov3/utils/ops_show_bbox.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 from PIL import ImageDraw
 
+import torchvision.transforms as transforms
 
 def show_bbox(img, bbox=None, names=None, xyxy=True, normalize=True):
 
@@ -26,10 +27,25 @@ def show_bbox(img, bbox=None, names=None, xyxy=True, normalize=True):
         if normalize:
             new_bbox[:, [0, 2]] *= img.size[0]
             new_bbox[:, [1, 3]] *= img.size[1]
-            # new_bbox[:, 2] *= img.size[0]
-            # new_bbox[:, 3] *= img.size[1]
     
     for bb in new_bbox:
         draw.rectangle(tuple(bb), outline='red')
 
     img.show()
+
+
+
+def show_tensor_bbox(image_tensor, bbox_tensor):
+    '''
+    image: c, h, w (totensor)
+    bboxes: n 6, [mask-0/1, label-0/cls, bbox-4/x/y/h/w]
+    '''
+    topil = transforms.ToPILImage()
+
+    image = topil(image_tensor)
+
+    bbox_tensor = bbox_tensor[bbox_tensor[:, 0] == 1] # filter mask=0, no object
+    bbox_tensor = bbox_tensor.cpu().data.numpy()
+
+    show_bbox(image, bbox_tensor[:, 2: ], names=bbox_tensor[:, 1], xyxy=False, normalize=True)
+
