1b846f70e1d51b3ad6083d9e11486a5183972e40
lyuwenyu <wenyu.lyu@gmail.com>
2018-09-15 18:42:22 +0800

    export onnx

--- yolov3/inference.py
+++ yolov3/inference.py
@@ -1,47 +1,76 @@
-import torch
 import numpy as np
-from PIL import Image, ImageDraw
-from model.build_model import DarkNet
-from torchvision import transforms
 import time
+from PIL import Image, ImageDraw
+
+import torch
+from torchvision import transforms
+
+from model.build_model import DarkNet
+from utils.ops_nms import NMS
+from utils.ops_show_bbox import show_bbox
+from utils import ops_transform
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', default='./_model/dog-cycle-car.png')
+parser.add_argument('--dim', type=int, default=416)
+args = parser.parse_args()
 
 def pre_image(img, inp_dim):
     totensor = transforms.ToTensor()
     img = Image.open(img).resize([inp_dim, inp_dim])
     return totensor(img).unsqueeze(0)
 
-dim = 608
-dim = 448
-dim = 416
-dim = 320
+# dim = 608
+# dim = 448
+# dim = 416
+# dim = 320
 # dim = 256 
 # dim = 224
+dim = args.dim
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cpu' # cuda' if torch.cuda.is_available() else 'cpu'
 device = torch.device(device)
 
-img_path = './_model/dog-cycle-car.png'
-image_pil = Image.open(img_path)
-image_pil = image_pil.resize((dim, dim))
-draw = ImageDraw.Draw(image_pil)
-
 model = DarkNet('./_model/yolov3.cfg', img_size=dim)
-# model.load_state_dict(torch.load('./model/yolov3.weights'))
+# model.load_state_dict(torch.load('./output/ckpt-00006'))
 model.load_weights('./model/yolov3.weights')
 model.eval()
 model = model.to(device=device)
 
-
-image = pre_image(img_path, dim)
+image = pre_image(args.path, dim)
 data = image.to(dtype=torch.float32, device=device)
 
 tic = time.time()
 pred = model(data.to(device=device))
 print('time: ', time.time()-tic)
 
-res = pred[pred[:, :, 4] > 0.99]
+show_bbox(Image.open(args.path).resize((dim, dim)), pred[pred[:, :, 0] > 0.3].cpu().data.numpy()[:, 1:5], xyxy=False, normalized=False)
 
-for p in res.cpu().data.numpy():
-    draw.rectangle((p[0]-p[2]/2, p[1]-p[3]/2, p[0]+p[2]/2, p[1]+p[3]/2), outline='red')
+pred = pred.cpu().data.numpy()[0]
+result = NMS(pred, objectness_threshold=0.3, iou_threshold=0.8)
 
-image_pil.show()
+for i in result:
+    show_bbox(Image.open(args.path).resize((dim, dim)), result[i][0])
+
+# print('++++++++++++++++++++++++++++++++++++++++')
+# import torch.onnx
+# import caffe2.python.onnx.backend as backbend
+# import onnx
+
+# dummy_input = torch.zeros(1, 3, args.dim, args.dim)
+# torch.onnx.export(model, dummy_input, 'test.onnx', verbose=True)
+# model = onnx.load('test.onnx')
+# onnx.checker.check_model(model)
+# onnx.helper.printable_graph(model.graph) 
+
+# rep = backbend.prepare(model, device='CPU')
+
+# outs = rep.run(image.cpu().data.numpy().astype(np.float32))
+# outs = outs[0][0]
+
+# print(type(outs))
+# print(outs[0].shape)
+
+# show_bbox(Image.open(args.path).resize((dim, dim)), outs[outs[:, 0] > 0.3][:, 1:5], xyxy=False, normalized=False)
