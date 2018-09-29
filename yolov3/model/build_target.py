ef8c5dae49128a5bed08bf16f5c56783875c4863
lyuwenyu <wenyu.lyu@gmail.com>
2018-09-29 12:04:29 +0800

    remove target unique

--- yolov3/model/build_target.py
+++ yolov3/model/build_target.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import random
 
-def bbox_iou(box1, box2, x1y1x2y2=True):
+def bbox_overlap(box1, box2, x1y1x2y2=True):
     '''
     '''
     if x1y1x2y2:
@@ -22,12 +22,12 @@ def bbox_iou(box1, box2, x1y1x2y2=True):
 
     inter_area = torch.clamp((x2 - x1 + 1), min=0) * torch.clamp((y2 - y1 + 1), min=0)
 
-    b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
-    b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
-     
-    iou = inter_area / (b1_area + b2_area - inter_area + le-16)
+    # b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+    # b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
 
-    return iou
+    # iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
+
+    return inter_area
 
 
 def _build_target(pred_boxes, pred_conf, pred_cls, target, scaled_anchors, nA, nC, nG, requestPrecision):
@@ -67,60 +67,29 @@ def _build_target(pred_boxes, pred_conf, pred_cls, target, scaled_anchors, nA, n
         gi = torch.clamp(gx.long(), min=0, max=nG-1)
         gj = torch.clamp(gy.long(), min=0, max=nG-1)
         
-        box_t = t[:, 3: ] * nG  # w, h: (n_gt, 2)
+        box_t = t[:, 1: ] * nG  # w, h: (n_gt, 2)
         box_a = scaled_anchors.unsqueeze(1).repeat(1, nTb, 1)  # w, h: (nA, n_gt, 2)
+        overlap = bbox_overlap(box_t, box_a, x1y1x2y2=False)
 
         inter_area = torch.min(box_t, box_a).prod(2)
         iou_anchor = inter_area / (gw * gh + box_a.prod(2) - inter_area + 1e-15)
 
         # ignore 
         _ignore = iou_anchor > ignore_threshold
-        _a = torch.arange(nA).view(3, 1).repeat(1, nTb).to(dtype=torch.long, device=pred_boxes.device)[_ignore]
-        _gi = gi.repeat(nA, 1)[_ignore]
-        _gj = gj.repeat(nA, 1)[_ignore]
-        conf_mask[i, _a, _gj, _gi] = 0
+        # _a = torch.arange(nA).view(3, 1).repeat(1, nTb).to(dtype=torch.long, device=pred_boxes.device)[_ignore]
+        # _gi = gi.repeat(nA, 1)[_ignore]
+        # _gj = gj.repeat(nA, 1)[_ignore]
+        # conf_mask[i, _a, _gj, _gi] = 0
+        # print(iou_anchor.shape)
+        # print(_ignore.max(0), _ignore.max(0).shape)
+        # print(_ignore.max(1), _ignore.max(1).shape)
+        # c += 1
 
-        iou_anchor_best, a = iou_anchor.max(0) # best anchor for each target.
+        conf_mask[i, _ignore.max(0), gj[_ignore.max(1)], gi[_ignore.max(1)]]
+
+        _, a = iou_anchor.max(0) # best anchor for each target.
+        
         # two targets can not claim the same anchor.
-        if nTb > 1:
-            _, iou_order = torch.sort(iou_anchor_best, descending=True)
-
-            # u = gi.float() * 0.3425405 + gj.float() * 0.2343235 + a.float() * 0.6462432
-            # _, uindex = np.unique(u[iou_order], return_index=True) # using numpy function
-
-            # if torch.__version__ >= '0.4.1': 
-            #     uniq, inverse_indices = torch.unique(u[iou_order], return_inverse=True)
-            # else:
-            #     uniq, inverse_indices = torch.unique(u.cpu()[iou_order.cpu()], return_inverse=True)
-            
-            # uindex = torch.zeros(len(uniq))
-            # for i, _u in enumerate(uniq):
-            #     uindex[i] = (u == _u)
-
-            # uindex = inverse_indices[: len(uniq)]
-
-            u = torch.cat((gi, gj, a), dim=0).view(3, -1)
-            _, uindex = np.unique(u[:, iou_order], axis=1, return_index=True)
-            
-            # print(uindex, _uindex)
-            # c += 1
-
-            k = iou_order[uindex]
-            k = k[iou_anchor_best[k] > 0.01]
-            if len(k) == 0: continue
-
-            a = a[k]
-            gi = gi[k]
-            gj = gj[k]
-            gx = gx[k]
-            gy = gy[k]
-            gw = gw[k]
-            gh = gh[k]
-            tc = tc[k]
-
-        else:
-            if iou_anchor_best < 0.01: continue
-            k = 0
 
         ### one box prior for each gt.
 
@@ -196,22 +165,23 @@ def build_target(pred_boxes, pred_conf, pred_cls, target, scaled_anchors, nA, nC
             conf_mask[b, iou_anchor > ignore_threshold, gj, gi] = 0
 
             # one target to one anchor exclusively.
-            # iou, a = iou_anchor.max(0) # best anchor for target.
-            ious, aindex = torch.sort(iou_anchor, descending=True)
-            a = -1
-            for ai, iou in zip(aindex, ious):
-                uid = gj.float() * 0.32432533 + gi.float() * 0.53243245 + ai.float() * 0.63321341
-                if (uid == used).sum() == 0 and iou > 0.5:
-                    used[t] = uid
-                    a = ai
-                    break
-                else:
-                    # print((uid == used).sum())
-                    # print('---using same anchor for one target---')
-                    pass  
-            if a == -1: # or used[t] == -1
-                # print('-----no anchor for this target------')
-                continue
+            _, a = iou_anchor.max(0) # best anchor for target.
+
+            # ious, aindex = torch.sort(iou_anchor, descending=True)
+            # a = -1
+            # for ai, iou in zip(aindex, ious):
+            #     uid = gj.float() * 0.32432533 + gi.float() * 0.53243245 + ai.float() * 0.63321341
+            #     if (uid == used).sum() == 0 and iou > 0.5:
+            #         used[t] = uid
+            #         a = ai
+            #         break
+            #     else:
+            #         # print((uid == used).sum())
+            #         # print('---using same anchor for one target---')
+            #         pass  
+            # if a == -1: # or used[t] == -1
+            #     # print('-----no anchor for this target------')
+            #     continue
 
             tx[b, a, gj, gi] = gx - gi.float()
             ty[b, a, gj, gi] = gy - gj.float()
