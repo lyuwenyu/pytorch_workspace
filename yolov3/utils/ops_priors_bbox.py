d4e4d0b2cb828125e5574df430fe73b720111f3e
lyuwenyu <wenyu.lyu@gmail.com>
2018-10-12 11:41:19 +0800

    prior bbox for ssd

--- yolov3/utils/ops_priors_bbox.py
+++ yolov3/utils/ops_priors_bbox.py
@@ -19,10 +19,10 @@ class PriorBox(object):
 
     def __call__(self, ):
         '''
+        generate prior bbox, [cx, cy, w, h] and normalized
+        return: torch.Tensor
         '''
-        
         anchors = []
-
         for i, grid in enumerate(self.grids):
             
             # center_x center_y
@@ -47,20 +47,21 @@ class PriorBox(object):
             whs = np.array(whs)
             # whs = torch.from_numpy(np.array(whs))
             
-            n = xx.shape[0] * xx.shape[1]
-            priors = np.zeros((n, len(whs), 4))
+            # n = xx.shape[0] * xx.shape[1]
+            priors = np.zeros((np.prod(cx.shape), len(whs), 4))
+            # print(priors.shape)
             # priors = torch.zeros((n, len(whs), 4))
             priors[:, :, 0] = cx.reshape(-1, 1)
             priors[:, :, 1] = cy.reshape(-1, 1)
             priors[:, :, 2:] = whs
 
             anchors += [priors.reshape(-1, 4)]
-            print(n, len(whs))
+            # print(np.prod(cx.shape) * len(whs))
 
         anchors = np.concatenate(anchors, axis=0)
         # anchors = torch.cat(anchors, dim=0)
-        print(anchors.shape)
-        anchors = torch.from_numpy(anchors)
+        # print(anchors.shape)
+        anchors = torch.from_numpy(anchors).to(dtype=torch.float)
         # anchors.clamp_(max=1., min=0.)
 
         return anchors
@@ -81,7 +82,7 @@ def bbox_overlap(box_a, box_b, x1y1x2y2=True):
     max_points = torch.min(box_a[:, 2:].unsqueeze(1).expand(A, B, 2),
                             box_b[:, 2:].unsqueeze(0).expand(A, B, 2))
 
-    inter = torch.clamp(max_points-min_points, min=0)
+    inter = torch.clamp(max_points - min_points, min=0)
 
     return inter.prod(dim=-1)
 
@@ -92,30 +93,83 @@ def jaccard(boxa, boxb):
     B X 4  e.g. [priors, 4]
     return A X B
     '''
-
     inter = bbox_overlap(boxa, boxb)
     area_a = ((boxa[:, 2] - boxa[:, 0]) * (boxa[:, 3] - boxa[:, 1])).unsqueeze(1).expand_as(inter)
     area_b = ((boxb[:, 2] - boxb[:, 0]) * (boxb[:, 3] - boxb[:, 1])).unsqueeze(0).expand_as(inter)
-
     union = area_a + area_b - inter
+
     return inter / union
 
 
-def match():
-    pass
+def xywh2xyxy(bbox):
+    '''
+    '''
+    new_bbox = torch.zeros_like(bbox)
+    new_bbox[:, 0] = bbox[:, 0] - bbox[:, 2] / 2
+    new_bbox[:, 1] = bbox[:, 1] - bbox[:, 3] / 2
+    new_bbox[:, 2] = bbox[:, 0] + bbox[:, 2] / 2
+    new_bbox[:, 3] = bbox[:, 1] + bbox[:, 3] / 2
 
-    
+    return new_bbox
+
+
+def match(gts, labels, priors, threshold=0.5):
+    '''
+    '''
+    # print(gts.shape)
+    # print(priors.shape)
+    # print(xywh2xyxy(priors).shape)
+    overlaps = jaccard(gts, xywh2xyxy(priors))
+
+    _, best_prior_idx = overlaps.max(1) # best prior for each gt
+    best_gt_overlap, best_gt_idx = overlaps.max(0) # best gt for each prior
+
+    best_gt_overlap.index_fill_(0, best_prior_idx, 1) # for threshold 
+    for j in range(best_prior_idx.size(0)): # 
+        best_gt_idx[best_prior_idx[j]] = j
+
+    matches = gts[best_gt_idx]
+
+    # print(len(best_gt_overlap<=0.5))
+    # print(torch.sum(best_gt_overlap<=0.5)) #HERE, sum difference to torch.sum
+    # print(sum(best_gt_overlap<=0.5))
+
+    # labels target
+    clss = labels[best_gt_idx] + 1
+    clss[best_gt_overlap < threshold] = 0 # bg
+
+    # center offet target
+    t_cxcy = (matches[:, :2] + matches[:, 2:]) / 2 - priors[:, :2]
+    t_cxcy /= priors[:, 2:] 
+
+    # w h target
+    t_wh = (matches[:, 2:] - matches[:, :2]) / priors[:, 2:]
+    t_wh = torch.log(t_wh + 1e-10)
+
+    # cx cy w h target
+    locs = torch.cat((t_cxcy, t_wh), dim=1)
+    print(locs.shape, clss.shape)
+
+    return locs, clss
+
 
 if __name__ == '__main__':
 
-    box1 = torch.rand(3, 4)
-    box2 = torch.rand(10, 4)
-
-    inters = bbox_overlap(box1, box2)
+    # box1 = torch.rand(3, 4)
+    # box2 = torch.rand(10, 4)
+    # inters = bbox_overlap(box1, box2)
     # print(inters.shape)
 
     priorbox = PriorBox()
+    priors = priorbox()
 
-    tic = time.time()
-    priorbox()
-    print(time.time() - tic)
+    # tic = time.time()
+    # print(priors[8000:8100])
+    # print(time.time() - tic)
+
+    gts = priors[:5]
+    labels = torch.arange(5)
+
+    locs, clss = match(gts, labels, priors)
+
+    
