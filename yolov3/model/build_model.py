d5f91dc7a4399393525c16fd05c7438b9dd9de38
lyuwenyu <wenyu.lyu@gmail.com>
2018-10-19 13:44:49 +0800

    add quad detection func.

--- yolov3/model/build_model.py
+++ yolov3/model/build_model.py
@@ -18,9 +18,13 @@ from utils.parse_config import parse_config
 from build_target import build_target, _build_target
 from utils.ops_logger import writer
 
+from build_textbox_layer import textbox_layer
+from build_yolo_layer import YOLOLayer
+from build_ssd_layer import SSDLayer
+
 VERSION = torch.__version__
 
-def create_model(model_defs, cls_num, img_dim=None):
+def create_model(model_defs, cls_num, img_dim=None, quad=False, yolo=False, ssd=False):
     '''
     '''
     hyperparameter = model_defs.pop(0)
@@ -77,7 +81,18 @@ def create_model(model_defs, cls_num, img_dim=None):
             else:
                 num_classes = int(defs['classes'])
 
-            yolo_layer = YOLOLayer(anchors, num_classes, anchor_index, img_dim)
+            if quad:
+                yolo_layer = textbox_layer(anchors, num_classes, anchor_index, img_dim)
+                attr_num = 4 + 8 + 1 + num_classes
+                # print(attr_num)
+
+            elif ssd:
+                yolo_layer = SSDLayer(num_classes)
+
+            else:
+                yolo_layer = YOLOLayer(anchors, num_classes, anchor_index, img_dim)
+                attr_num = 4 + 1 + num_classes
+
             module.add_module(f'yolo_{i}', yolo_layer)
 
             # 
@@ -87,7 +102,8 @@ def create_model(model_defs, cls_num, img_dim=None):
                 stride = m.stride
                 in_channels = m.in_channels
             
-            _m = nn.Conv2d(in_channels, (num_classes + 5) * len(anchors), kernel_size, stride)
+            _m = nn.Conv2d(in_channels, attr_num * len(anchors), kernel_size, stride)
+            print(_m)
             del module_list[-1] # __delitem__
             module_list += [nn.Sequential(OrderedDict([(name, _m)]))] # __iadd__ append
             # module_list[len(module_list) - 1] = nn.Sequential(OrderedDict([(name, _m)]))
@@ -104,141 +120,15 @@ class EmptyLayer(nn.Module):
     def __init__(self, ):
         super(EmptyLayer, self).__init__()
 
-#---
-class YOLOLayer(nn.Module):
-    def __init__(self, anchors, num_classes, anchor_index, img_dim=None):
-        super(YOLOLayer, self).__init__()
-        
-        self.anchors = anchors
-        self.nA = len(anchors)
-        self.nC = num_classes
-        self.bbox_attrs = num_classes + 5
-        self.max_grid = 100
-
-        if anchor_index[0] == 6:
-            stride = 32.
-        elif anchor_index[0] == 3:
-            stride = 16.
-        else:
-            stride = 8.
-
-        # self.max_grid = 100 # int(img_dim / stride)
-        self.stride = stride
-
-        self.scaled_anchors = torch.tensor(anchors) / stride   
-        self.anchor_w = self.scaled_anchors[:, 0]
-        self.anchor_h = self.scaled_anchors[:, 1]
-
-        if VERSION >= '0.4.1':
-            self.grid_h, self.grid_w = torch.meshgrid((torch.arange(self.max_grid), torch.arange(self.max_grid)))
-        else:
-            self.grid_w = torch.arange(self.max_grid).repeat(self.max_grid, 1)
-            self.grid_h = torch.arange(self.max_grid).repeat(self.max_grid, 1).t()
-
-        self.mseLoss = nn.MSELoss() # size_average=True
-        self.bceLoss = nn.BCEWithLogitsLoss()
-        self.crossentropy = nn.CrossEntropyLoss()
-
-    def forward(self, p, target=None, requestPrecision=False, epoch=None):
-        '''forward '''
-        bs = p.shape[0]
-        nG = p.shape[2]
-        grid_x = self.grid_w[:nG, :nG].to(dtype=p.dtype, device=p.device)
-        grid_y = self.grid_h[:nG, :nG].to(dtype=p.dtype, device=p.device)
-        # grid_x = torch.arange(nG).repeat(nG, 1).to(dtype=p.dtype, device=p.device)
-        # grid_y = torch.arange(nG).repeat(nG, 1).t().to(dtype=p.dtype, device=p.device)
-
-        p = p.view(bs, self.nA, self.bbox_attrs, nG, nG).permute(0, 1, 3, 4, 2).contiguous()
-
-        x = torch.sigmoid(p[..., 0])
-        y = torch.sigmoid(p[..., 1])
-        w = p[..., 2]
-        h = p[..., 3]
-
-        width = torch.exp(w) * self.anchor_w.view(1, -1, 1, 1).to(dtype=w.dtype, device=w.device) #.data
-        height = torch.exp(h) * self.anchor_h.view(1, -1, 1, 1).to(dtype=h.dtype, device=h.device)
-
-        pred_boxes = torch.zeros((bs, self.nA, nG, nG, 4)).to(dtype=p.dtype, device=p.device)
-        
-        pred_conf = p[..., 4] 
-        pred_cls =  p[..., 5:] 
-
-        # cross entropy
-        # pred_cls = p[..., 5:]
-
-        if target is None: # inference phase
-            pred_boxes[..., 0] = x + grid_x.float()
-            pred_boxes[..., 1] = y + grid_y.float()
-            pred_boxes[..., 2] = width
-            pred_boxes[..., 3] = height
-
-            out = torch.cat((
-                pred_boxes.view(bs, -1, 4) * self.stride,
-                torch.sigmoid(pred_conf).view(bs, -1, 1),
-                torch.sigmoid(pred_cls).view(bs, -1, self.nC)),
-                # nn.functional.softmax(pred_cls, dim=-1).view(bs, -1, self.nC)), 
-                dim=-1)
-
-            return out
-        
-        else: # train phase TODO 
-
-            tx, ty, tw, th, tconf, tcls, conf_mask = build_target(pred_boxes.data, # here 
-                                                            pred_conf.data, 
-                                                            pred_cls.data, 
-                                                            target, 
-                                                            self.scaled_anchors.to(device=p.device), 
-                                                            self.nA, self.nC, nG,
-                                                            requestPrecision)
-            numobj = tconf.sum().float()
-            mask = tconf
-
-            if numobj > 0:
-
-                # pred_conf = torch.sigmoid(pred_conf)
-                # pred_cls =  torch.sigmoid(pred_cls)
-
-                lx = self.mseLoss(x[mask], tx[mask])
-                ly = self.mseLoss(y[mask], ty[mask])
-                lw = self.mseLoss(w[mask], tw[mask])
-                lh = self.mseLoss(h[mask], th[mask])
-
-                # lconf = self.bceLoss(pred_conf[conf_mask != 0], tconf[conf_mask != 0].to(dtype=pred_conf.dtype))
-                lconf_bg = self.bceLoss(pred_conf[conf_mask == -1], tconf[conf_mask == -1].to(dtype=pred_conf.dtype))
-                lconf_ob = self.bceLoss(pred_conf[conf_mask == 1], tconf[conf_mask == 1].to(dtype=pred_conf.dtype))
-                lconf = 2 * lconf_bg + lconf_ob # 2. * lconf_bg + lconf_ob
-
-                lcls = self.bceLoss(pred_cls[mask], tcls[mask])
-                # lcls = self.crossentropy(pred_cls[mask], tcls[mask].argmax(1))
-
-                # print(lx.item(), ly.item(), lw.item(), lh.item(), lcls.item(), lconf_bg.item(), lconf_ob.item(), lconf.item())
-
-            else:
-                lx, ly, lw, lh, lcls, lconf = [torch.tensor(0.).to(dtype=torch.float32, device=p.device)] * 6
-
-            loss = (lx + ly + lw + lh + lconf + lcls) * numobj / (bs + 1e-8)
-            
-            # writer.add_scalars('losses', {
-            #     'loss': loss.item(),
-            #     'lx': lx.item(),
-            #     'ly': ly.item(),
-            #     'lw': lw.item(),
-            #     'lh': lh.item(),
-            #     'lcong': lconf.item(),
-            #     'lcls': lcls.item()
-            # })
-
-            return loss, lx, ly, lw, lh, lconf, lcls
-
 
 #---
 class DarkNet(nn.Module):
-    def __init__(self, cfg, cls_num, img_dim=None):
+    def __init__(self, cfg, cls_num, quad=False):
         super(DarkNet, self).__init__()
         
         self.module_defs = parse_config(cfg)
         # self.module_defs[0]['height'] = img_size
-        self.module_list = create_model(self.module_defs, cls_num=cls_num, img_dim=img_dim)
+        self.module_list = create_model(self.module_defs, cls_num=cls_num, quad=quad)
 
         self.loss_names = ['loss', 'x', 'y', 'w', 'h', 'conf', 'cls' ]
         
@@ -287,7 +177,8 @@ class DarkNet(nn.Module):
             return torch.cat(outputs, dim=1)
 
         else:
-            return sum(outputs)
+            # print(outputs)
+            return sum(outputs) # 
 
 
     def set_target(self, target):
@@ -296,8 +187,11 @@ class DarkNet(nn.Module):
         for ii in range(target.shape[0]):
             _target = target[ii]
             _target = _target[_target[:, 0] == 1]
+            # if _target.shape[0] == 0:
+            #     continue
             bboxes += [_target[:, 1:]]
         return bboxes
+
 
     def load_weights(self, weights_path):
         """Parses and loads the weights stored in 'weights_path'"""
@@ -357,15 +251,16 @@ if __name__ == '__main__':
     path = os.path.join(path, '..', '_model', 'yolov3.cfg')
     
     data = torch.rand(2, 3, 416, 416).to(device=torch.device('cuda'))
-    model = DarkNet(path, img_size=416)
-    model.load_state_dict(torch.load(os.path.join(filedir, 'yolov3.torch')))
-    model.eval()
-    model = model.cuda()
+    model = DarkNet(path, cls_num=3, quad=True)
+    print(model)
+    # model.load_state_dict(torch.load(os.path.join(filedir, 'yolov3.torch')))
+    # model.eval()
+    # model = model.cuda()
 
-    # model.load_weights(weights_path=os.path.join(filedir, 'yolov3.weights'))
-    torch.save(model.state_dict(), os.path.join(filedir, 'yolov3.torch'))
+    model.load_weights(weights_path=os.path.join('/home/wenyu/workspace/pytorch_workspace/yolov3/_model', 'yolov3.weights'))
+    # torch.save(model.state_dict(), os.path.join(filedir, 'yolov3.torch'))
 
-    tic = time.time()
-    print(model(data).shape)
-    print(time.time() - tic)
+    # tic = time.time()
+    # print(model(data).shape)
+    # print(time.time() - tic)
 
--- yolov3/model/build_target.py
+++ yolov3/model/build_target.py
@@ -2,33 +2,6 @@ import torch
 import numpy as np
 import random
 
-def bbox_overlap(box1, box2, x1y1x2y2=True):
-    '''
-    '''
-    if x1y1x2y2:
-        b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
-        b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
-    
-    else:
-        b1_x1, b1_x2 = box1[:, 0] - box1[:, 2] / 2, box1[:, 0] + box1[:, 2] / 2
-        b1_y1, b1_y2 = box1[:, 1] - box1[:, 3] / 2, box1[:, 1] + box1[:, 3] / 2
-        b2_x1, b2_x2 = box2[:, 0] - box2[:, 2] / 2, box2[:, 0] + box2[:, 2] / 2
-        b2_y1, b2_y2 = box2[:, 1] - box2[:, 3] / 2, box2[:, 1] + box2[:, 3] / 2
-
-    x1 = torch.max(b1_x1, b2_x1)
-    y1 = torch.max(b1_y1, b2_y1)
-    x2 = torch.min(b1_x2, b2_x2)
-    y2 = torch.min(b1_y2, b2_y2)
-
-    inter_area = torch.clamp((x2 - x1 + 1), min=0) * torch.clamp((y2 - y1 + 1), min=0)
-
-    # b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
-    # b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
-
-    # iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
-
-    return inter_area
-
 
 def _build_target(pred_boxes, pred_conf, pred_cls, target, scaled_anchors, nA, nC, nG, requestPrecision):
     '''
@@ -141,7 +114,7 @@ def build_target(pred_boxes, pred_conf, pred_cls, target, scaled_anchors, nA, nC
 
     for b in range(nB):
 
-        used = torch.zeros(nT[b]).fill_(-1).to(device=pred_boxes.device)
+        # used = torch.zeros(nT[b]).fill_(-1).to(device=pred_boxes.device)
         # bbx_gt_index = range(nT[b])
         # random.shuffle(bbx_gt_index)
         # for t in range(bbx_gt_index):
@@ -160,6 +133,9 @@ def build_target(pred_boxes, pred_conf, pred_cls, target, scaled_anchors, nA, nC
 
             inter_area = torch.min(gbox, scaled_anchors).prod(1)
             iou_anchor = inter_area / (gbox.prod(0) + scaled_anchors.prod(1) - inter_area + 1e-15)
+
+            # print(iou_anchor)
+            # c+=1
 
             # conf_mask[b, iou_anchor < ignore_threshold] = -1
             conf_mask[b, iou_anchor > ignore_threshold, gj, gi] = 0
@@ -196,10 +172,121 @@ def build_target(pred_boxes, pred_conf, pred_cls, target, scaled_anchors, nA, nC
     return tx, ty, tw, th, tconf, tcls, conf_mask
 
 
+def build_quad_target(pred_boxes, target, scaled_anchors, nA, nC, nG, requestPrecision):
+    '''
+    target: n, t, [c, x, y, w, h]
+
+    nGT, nCorrect, tx, ty, tw, th, tconf, tcls
+    '''
+    ignore_threshold = 0.5
+
+    nB = len(target)
+    nT = [len(bs) for bs in target]
+    # maxnT = max(nT)
+
+    tx = torch.zeros(nB, nA, nG, nG).to(device=pred_boxes.device)
+    ty = torch.zeros(nB, nA, nG, nG).to(device=pred_boxes.device)
+    tw = torch.zeros(nB, nA, nG, nG).to(device=pred_boxes.device)
+    th = torch.zeros(nB, nA, nG, nG).to(device=pred_boxes.device)
+
+    txp1 = torch.zeros(nB, nA, nG, nG).to(dtype=pred_boxes.dtype, device=pred_boxes.device)
+    typ1 = torch.zeros(nB, nA, nG, nG).to(dtype=pred_boxes.dtype, device=pred_boxes.device)
+    txp2 = torch.zeros(nB, nA, nG, nG).to(dtype=pred_boxes.dtype, device=pred_boxes.device)
+    typ2 = torch.zeros(nB, nA, nG, nG).to(dtype=pred_boxes.dtype, device=pred_boxes.device)
+    txp3 = torch.zeros(nB, nA, nG, nG).to(dtype=pred_boxes.dtype, device=pred_boxes.device)
+    typ3 = torch.zeros(nB, nA, nG, nG).to(dtype=pred_boxes.dtype, device=pred_boxes.device)
+    txp4 = torch.zeros(nB, nA, nG, nG).to(dtype=pred_boxes.dtype, device=pred_boxes.device)
+    typ4 = torch.zeros(nB, nA, nG, nG).to(dtype=pred_boxes.dtype, device=pred_boxes.device)
+
+    tcls = torch.zeros(nB, nA, nG, nG, nC).to(device=pred_boxes.device) #.fill_(0)
+    # print(tcls.shape)
+    tconf = torch.ByteTensor(nB, nA, nG, nG).fill_(0).to(device=pred_boxes.device) # mask
+
+    # conf_mask = torch.zeros(nB, nA, nG, nG).fill_(0).to(device=pred_boxes.device)
+    conf_mask = torch.zeros(nB, nA, nG, nG).fill_(-1).to(device=pred_boxes.device)
+
+    nGt = 0
+
+    for b in range(nB):
+
+        for t in range(nT[b]):
+
+
+            tc = target[b][t, 0].long()
+            p1 = target[b][t, [1, 2]] * nG
+            p2 = target[b][t, [3, 4]] * nG
+            p3 = target[b][t, [5, 6]] * nG
+            p4 = target[b][t, [7, 8]] * nG
+
+            xmin = torch.min(target[b][t, [1, 3, 5, 7]]) * nG
+            xmax = torch.max(target[b][t, [1, 3, 5, 7]]) * nG
+            ymin = torch.min(target[b][t, [2, 4, 6, 8]]) * nG
+            ymax = torch.max(target[b][t, [2, 4, 6, 8]]) * nG
+
+            # assert xmin == p1[0], ''
+            # assert xmax == p3[0], ''
+            # assert ymin == p1[1], ''
+            # assert ymax == p3[1], ''
+
+            gx = (xmin + xmax) / 2.
+            gy = (ymin + ymax) / 2.
+            gw = xmax - xmin
+            gh = ymax - ymin
+
+            # gbox = torch.cat((gw.view(1, -1), gh.view(1, -1)), dim=-1).view(2) # target[b][t, 3:] * nG
+            # print(gbox.shape)
+            gi = torch.clamp(gx.long(), min=0, max=nG - 1)
+            gj = torch.clamp(gy.long(), min=0, max=nG - 1)
+
+            inter_area = torch.min(gw, scaled_anchors[:, 0]) * torch.min(gh, scaled_anchors[:, 1])
+            iou_anchor = inter_area / (gw * gh + scaled_anchors.prod(1) - inter_area + 1e-15)
+
+            # conf_mask[b, iou_anchor < ignore_threshold] = -1
+            conf_mask[b, iou_anchor > ignore_threshold, gj, gi] = 0
+
+            # one target to one anchor exclusively.
+            _, a = iou_anchor.max(0) # best anchor for target.
+
+            tx[b, a, gj, gi] = gx - gi.float()
+            ty[b, a, gj, gi] = gy - gj.float()
+
+            tw[b, a, gj, gi] = torch.log(gw / scaled_anchors[a, 0] + 1e-15)
+            th[b, a, gj, gi] = torch.log(gh / scaled_anchors[a, 1] + 1e-15)
+
+            # anchor clockwise points
+            aw = scaled_anchors[a, 0]
+            ah = scaled_anchors[a, 1]
+            ap1x = gi.float() - aw / 2
+            ap1y = gj.float() - ah / 2
+            ap2x = gi.float() + aw / 2
+            ap2y = gj.float() - ah / 2
+            ap3x = gi.float() + aw / 2
+            ap3y = gj.float() + ah / 2            
+            ap4x = gi.float() - aw / 2
+            ap4y = gj.float() + ah / 2 
+
+            # points target
+            txp1[b, a, gj, gi] = (p1[0] - ap1x) / aw
+            typ1[b, a, gj, gi] = (p1[1] - ap1y) / ah
+            txp2[b, a, gj, gi] = (p2[0] - ap2x) / aw
+            typ2[b, a, gj, gi] = (p2[1] - ap2y) / ah
+            txp3[b, a, gj, gi] = (p3[0] - ap3x) / aw
+            typ3[b, a, gj, gi] = (p3[1] - ap3y) / ah
+            txp4[b, a, gj, gi] = (p4[0] - ap4x) / aw
+            typ4[b, a, gj, gi] = (p4[1] - ap4y) / ah
+
+            tcls[b, a, gj, gi, tc] = 1
+            tconf[b, a, gj, gi] = 1
+            conf_mask[b, a, gj, gi] = 1
+
+    return tx, ty, tw, th, txp1, typ1, txp2, typ2, txp3, typ3, txp4, typ4, tconf, tcls, conf_mask
+
+
+
 
 if __name__ == '__main__':
 
     target = torch.rand(2, 2, 5)
     scaled_anchors = torch.rand(3, 2)
 
-    build_target(0, 0, 0, target, scaled_anchors, 3, 80, 13, False)
+
