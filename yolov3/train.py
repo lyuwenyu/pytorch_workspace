e4a717429b729c8709f398e8dc3b17305bc8d009
lyuwenyu <wenyu.lyu@gmail.com>
2018-11-22 16:28:17 +0800

    fix save bugs

--- yolov3/train.py
+++ yolov3/train.py
@@ -17,20 +17,21 @@ import argparse
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 parser = argparse.ArgumentParser()
 parser.add_argument('--device_ids', type=list, default=[0, 1])
-parser.add_argument('--num_classes', type=int, default=20)
+parser.add_argument('--num_classes', type=int, default=10)
 parser.add_argument('--resume_path', type=str, default='') # ./output/state-ckpt-epoch-00000
 parser.add_argument('--loss_step', type=int, default=10)
-parser.add_argument('--save_step', type=int, default=5)
-parser.add_argument('--img_dims', type=list, default=[416, ])
-parser.add_argument('--batch_sizes', type=list, default=[32, ])
+parser.add_argument('--save_step', type=int, default=10)
+parser.add_argument('--img_dims', type=list, default=[416, 320, 512]) # 608, 320
+parser.add_argument('--batch_sizes', type=list, default=[16, 24, 12]) # 16, 48
 parser.add_argument('--num_workers', type=list, default=8)
 parser.add_argument('--epoches', type=int, default=201)
-parser.add_argument('--model_cfg', type=str, default='./_model/yolov3.cfg')
-parser.add_argument('--lr', type=float, default=0.0001)
-parser.add_argument('--momentum', type=float, default=0.95)
-parser.add_argument('--milestones', type=list, default=[80, 150, 180])
+parser.add_argument('--model_cfg', type=str, default='./_model/yolov3_v1.cfg')
+parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--momentum', type=float, default=0.9)
+parser.add_argument('--milestones', type=list, default=[100, 150, 180])
 parser.add_argument('--gamma', type=float, default=0.1)
 args = parser.parse_args()
+
 
 def train(model, dataloader, optimizer, scheduler, epoch=0):
     '''train'''
@@ -45,9 +46,13 @@ def train(model, dataloader, optimizer, scheduler, epoch=0):
         targets = targets.to(torch.device(device))
 
         tic = time.time()
-        # losses = model(images, targets)
-        losses = nn.parallel.data_parallel(module=model, inputs=(images, targets), device_ids=args.device_ids, dim=0)
-        losses = losses.sum()
+        
+        # if 'cuda' in device and len(args.device_ids) > 1:
+        #     losses = nn.parallel.data_parallel(module=model, inputs=(images, targets), device_ids=args.device_ids, dim=0)
+        #     losses = losses.sum()
+        # else:
+        #     losses = model(images, targets)
+        losses = model(images, targets)
         
         optimizer.zero_grad()
         losses.backward()
@@ -62,16 +67,16 @@ def train(model, dataloader, optimizer, scheduler, epoch=0):
 
             step_losses = np.array(step_losses).mean(axis=0)
             # lin = ', '.join(['{:0.6f}'.format(ls) for ls in step_losses])
-            lin = 'epoch/iter: {:0>3}/{:0>3}, lr: {}, time: {:.4f}s, losses: {:.4f}'.format(e, i + 1,
+            lin = 'epoch/iter: {:0>3}/{:0>3}, lr: {}, time: {:.4f}s, losses: {:.4f}'.format(epoch, i + 1,
                                                                                 format(optimizer.param_groups[0]['lr'],'.0e'),
                                                                                 np.array(step_time).mean(),
                                                                                 step_losses)
             step_losses, step_time = [], []
             print(lin)
             
-    if e % args.save_step == 0:
+    if epoch % args.save_step == 0:
         state = {
-            'epoch': e,
+            'epoch': epoch,
             'model': model.state_dict(),
             'optimizer': optimizer.state_dict(),
             'scheduler': scheduler.state_dict()
@@ -95,7 +100,7 @@ if __name__ == '__main__':
     dataloaders = [data.DataLoader(d, batch_size=bs, shuffle=True, num_workers=args.num_workers) for d, bs in zip(datasets, args.batch_sizes)]
 
     model = DarkNet(args.model_cfg, cls_num=args.num_classes)
-    # model.load_weights('./_model/yolov3.weights')
+    model.load_weights('./_model/yolov3.weights')
     model = model.to(torch.device(device))
 
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
@@ -104,13 +109,24 @@ if __name__ == '__main__':
     if args.resume_path:
         ops_weight_init.resume(model, optimizer, scheduler, args.resume_path)
 
-    for e in range(scheduler.last_epoch + 1, int(args.epoches / len(dataloaders) + 1)):
+    # for e in range(scheduler.last_epoch + 1, int(args.epoches / len(dataloaders) + 1)):
+    #     if e > 10: 
+    #         random.shuffle(dataloaders)
+    #     for i, dtloader in enumerate(dataloaders):
+    #         train(model, dtloader, optimizer, scheduler, e * len(dataloaders) + i)
 
-        random.shuffle(dataloaders)
-        
-        for i, dtloader in enumerate(dataloaders):
-            train(model, dtloader, optimizer, scheduler, e * len(dataloaders) + i)
-        
+    e = scheduler.last_epoch + 1
+
+    while e < args.epoches:
+        if e < 10: 
+            train(model, dataloaders[0], optimizer, scheduler, e)
+            e += 1
+        else:
+            random.shuffle(dataloaders)
+            for i, dtloader in enumerate(dataloaders):
+                train(model, dtloader, optimizer, scheduler, e + i)
+            e += len(dataloaders)
+
         # train(model, dataloader, optimizer, scheduler, epoch=e)
         
         # validate(model, dataloader, epoch=e)
