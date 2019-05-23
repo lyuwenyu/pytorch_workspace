df3546fe0569d125390068d60561a5f0e02b6082
lyuwenyu <wenyu.lyu@gmail.com>
2019-05-23 18:20:38 +0800

    pytorch dataset

---
+++ pytorch-tutorial/dataset.py
@@ -0,0 +1,20 @@
+"""
+by. wenyu
+reference: https://discuss.pytorch.org/t/torch-utils-data-dataset-random-split/32209/4
+"""
+
+import torch
+import torch.utils.data as data
+
+
+
+
+class Init_dataset(data.Dataset):
+    pass
+
+
+
+
+
+def get_dataloader():
+    pass
