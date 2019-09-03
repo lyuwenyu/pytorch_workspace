3922e579520035743ca5f4c538b5c61de3cbedf3
lyuwenyu <wenyu.lyu@gmail.com>
2019-09-03 14:24:36 +0800

    update dataset

--- pytorch-tutorial/dataset.py
+++ pytorch-tutorial/dataset.py
@@ -1,20 +1,32 @@
 """
 by. wenyu
-reference: https://discuss.pytorch.org/t/torch-utils-data-dataset-random-split/32209/4
+reference: 
+1. https://discuss.pytorch.org/t/torch-utils-data-dataset-random-split/32209/4
+2. https://github.com/IgorSusmelj/pytorch-styleguide/issues/5#
+
 """
 
 import torch
 import torch.utils.data as data
 
+from prefetch_generator import BackgroundGenerator
 
 
+class DataloaderX(data.DataLoader):
+    def __iter__(self):
+        return BackgroundGenerator(super().__iter__())
+    
 
-class Init_dataset(data.Dataset):
-    pass
-
-
-
-
-
-def get_dataloader():
-    pass
+class Dataset(DataloaderX):
+    def __init__(self):
+        pass
+    
+    def __len__(self, ):
+        '''
+        '''
+        pass
+    
+    def __getitem__(self, i):
+        '''
+        '''
+        pass
