88ba8c9a65838dcf887539f0516e821ca711b653
lyuwenyu <wenyu.lyu@gmail.com>
2019-09-03 14:30:41 +0800

    update

--- pytorch-tutorial/torchstat_test.py
+++ pytorch-tutorial/torchstat_test.py
@@ -1,6 +1,9 @@
 from torchstat import stat
+from torchsummary import summary
 import torchvision.models as models
-
 
 model = models.resnet18()
 stat(model, (3, 224, 224))
+
+model = models.vgg13_bn()
+summary(model, (3, 224 ,224))
--- pytorch-tutorial/torchsummary_test.py
+++
@@ -1,6 +0,0 @@
-import torchvision.models as models
-from torchsummary import summary
-
-vgg = models.vgg13_bn()
-
-summary(vgg, (3, 224 ,224))
